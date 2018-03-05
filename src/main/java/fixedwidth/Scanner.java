dadad1b9c9d6ffe0ea399dd429747a33bbd668cb
Wouter Oet <wouter.oet@gmail.com>
2018-03-05 08:07:49 +0100

    Support for date patterns

--- src/main/java/fixedwidth/Scanner.java
+++ src/main/java/fixedwidth/Scanner.java
@@ -3,35 +3,53 @@ package fixedwidth;
 import fixedwidth.annotations.Converter;
 import fixedwidth.annotations.Position;
 import fixedwidth.annotations.Record;
+import fixedwidth.annotations.WithPattern;
 
 import java.lang.reflect.Field;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 class Scanner {
 
-    private static final Map<Class<?>, Function<String, Object>> SUPPORTED_TYPES = new HashMap<>();
+    private static final Map<Class<?>, Function<Field, Function<String, Object>>> SUPPORTED_TYPES = new HashMap<>();
+
+    private static Function<Field, Function<String, Object>> withPatternContent(BiFunction<String, DateTimeFormatter, Object> consumer, DateTimeFormatter formatter) {
+        return field -> {
+            WithPattern pattern = field.getAnnotation(WithPattern.class);
+            if (pattern != null) {
+                return s -> consumer.apply(s, DateTimeFormatter.ofPattern(pattern.value()));
+            } else {
+                return s -> consumer.apply(s, formatter);
+            }
+        };
+    }
 
     static {
-        SUPPORTED_TYPES.put(Long.class, Long::parseLong);
-        SUPPORTED_TYPES.put(long.class, Long::parseLong);
-        SUPPORTED_TYPES.put(Integer.class, Integer::parseInt);
-        SUPPORTED_TYPES.put(int.class, Integer::parseInt);
-        SUPPORTED_TYPES.put(Short.class, Short::parseShort);
-        SUPPORTED_TYPES.put(short.class, Short::parseShort);
-        SUPPORTED_TYPES.put(String.class, s -> s);
-        SUPPORTED_TYPES.put(LocalDateTime.class, LocalDateTime::parse);
-        SUPPORTED_TYPES.put(LocalDate.class, LocalDate::parse);
-        SUPPORTED_TYPES.put(Double.class, Double::parseDouble);
-        SUPPORTED_TYPES.put(double.class, Double::parseDouble);
-        SUPPORTED_TYPES.put(Float.class, Float::parseFloat);
-        SUPPORTED_TYPES.put(float.class, Float::parseFloat);
+        Function<Function<String, Object>, Function<Field, Function<String, Object>>> withoutContext = i -> f -> i;
+
+        SUPPORTED_TYPES.put(Long.class, withoutContext.apply(Long::parseLong));
+        SUPPORTED_TYPES.put(long.class, withoutContext.apply(Long::parseLong));
+        SUPPORTED_TYPES.put(Integer.class, withoutContext.apply(Integer::parseInt));
+        SUPPORTED_TYPES.put(int.class, withoutContext.apply(Integer::parseInt));
+        SUPPORTED_TYPES.put(Short.class, withoutContext.apply(Short::parseShort));
+        SUPPORTED_TYPES.put(short.class, withoutContext.apply(Short::parseShort));
+        SUPPORTED_TYPES.put(String.class, withoutContext.apply(s -> s));
+        SUPPORTED_TYPES.put(Double.class, withoutContext.apply(Double::parseDouble));
+        SUPPORTED_TYPES.put(double.class, withoutContext.apply(Double::parseDouble));
+        SUPPORTED_TYPES.put(Float.class, withoutContext.apply(Float::parseFloat));
+        SUPPORTED_TYPES.put(float.class, withoutContext.apply(Float::parseFloat));
+
+        SUPPORTED_TYPES.put(LocalDateTime.class, withPatternContent(LocalDateTime::parse, DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        SUPPORTED_TYPES.put(LocalDate.class, withPatternContent(LocalDate::parse, DateTimeFormatter.ISO_LOCAL_DATE));
+
     }
 
     private final Class<?> clazz;
@@ -60,6 +78,22 @@ class Scanner {
 
     private Mapping createMapping(Field field) {
         field.setAccessible(true);
+        Position position = getPosition(field);
+
+        Converter converter = field.getAnnotation(Converter.class);
+        if (converter != null) {
+            return useConverter(field, position, converter);
+        }
+
+        Function<Field, Function<String, Object>> contentFunction = SUPPORTED_TYPES.get(field.getType());
+        if (contentFunction == null) {
+            throw RecordDefinitionException.unsupportedType(field);
+        }
+
+        return new Mapping(field, position.start(), position.end(), contentFunction.apply(field));
+    }
+
+    private Position getPosition(Field field) {
         Position position = field.getAnnotation(Position.class);
         if (position == null) {
             throw RecordDefinitionException.forMissingPosition(field);
@@ -67,18 +101,7 @@ class Scanner {
         if (position.start() >= position.end()) {
             throw RecordDefinitionException.invalidPosition(field);
         }
-
-        Converter converter = field.getAnnotation(Converter.class);
-        if (converter != null) {
-            return useConverter(field, position, converter);
-        }
-
-        Function<String, Object> function = SUPPORTED_TYPES.get(field.getType());
-        if (function == null) {
-            throw RecordDefinitionException.unsupportedType(field);
-        }
-
-        return new Mapping(field, position.start(), position.end(), function);
+        return position;
     }
 
     private Mapping useConverter(Field field, Position position, Converter converter) {
